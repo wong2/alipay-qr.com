3a99bf9ba56ec9c9febb5af0016398a153c4ca2e
wong2 <wonderfuly@gmail.com>
2014-05-10 01:25:17 +0800

    minify js

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,13 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     mincss = require('gulp-minify-css');
 
+gulp.task('js', function() {
+  gulp.src(['static/js/bootstrap.file-input.js'])
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./static/js/'))
+});
+
 gulp.task('validator.js', function() {
   gulp.src(['static/js/bootstrapValidator.js', 'static/js/qrdecoder.js', 'qrdecoder.js/qrcode.validator.js'])
     .pipe(uglify())
@@ -12,10 +19,10 @@ gulp.task('validator.js', function() {
 });
 
 gulp.task('css', function() {
-  gulp.src('static/css/*.css')
+  gulp.src(['static/css/flat-ui.css'])
     .pipe(mincss())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./static/css/'))
 });
 
-gulp.task('default', ['validator.js', 'css'])
+gulp.task('default', ['js', 'validator.js', 'css'])
