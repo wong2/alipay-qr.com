8e85ad5c8c797d8f75a873b23ceaecfff4e1b6fd
wong2 <wonderfuly@gmail.com>
2014-05-17 15:26:28 +0800

    update config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 #-*-coding:utf-8-*-
 
-SECRET_KEY = 'THIS IS AN INSECURE SECRET'
+SECRET_KEY = ''
 UPLOAD_FOLDER = 'qr_images' # inside ./static/
 STATIC_PREFIX = '/static'
 
 REDIS_URL = 'redis://localhost:6379/5'
 
-WEIBO_KEY  = '1575471143'
-WEIBO_SECRET = '65bd9347098234c912b47922070971c6'
+WEIBO_KEY  = ''
+WEIBO_SECRET = ''
 
 try:
     from local_config import *
